fdc656163072796783e129933df2185535221d84
ianrahman <ian.alexander.rahman@gmail.com>
2016-03-30 17:51:18 -0400

    Added ActivityCard class, changed name of primary view controller, and set project team to Flatiron School

---
+++ EggplantButton/ActivityCard.m
@@ -0,0 +1,21 @@
+//
+//  ActivityCard.m
+//  EggplantButton
+//
+//  Created by Ian Alexander Rahman on 3/30/16.
+//  Copyright © 2016 Team Eggplant Button. All rights reserved.
+//
+
+#import "ActivityCard.h"
+
+@implementation ActivityCard
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ EggplantButton/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  EggplantButton
+//
+//  Created by Ian Alexander Rahman on 3/30/16.
+//  Copyright © 2016 Team Eggplant Button. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
