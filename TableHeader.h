3b1e647c5520ebc122866a19144499b3d983e1b8
sguevara11 <stephanie.n.guevara@gmail.com>
2016-05-03 17:19:08 -0400

    added section header to table

---
+++ TableHeader.h
@@ -0,0 +1,16 @@
+//
+//  TableHeader.h
+//  EasyOut
+//
+//  Created by Stephanie on 5/3/16.
+//  Copyright © 2016 Team Eggplant Button. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableHeader : UIView
+
+
+@property (strong, nonatomic) IBOutlet TableHeader *view;
+
+@end
